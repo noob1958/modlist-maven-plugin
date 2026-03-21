b30e43f1f412f61caaa00bf1c9a0927126a8bc12
noob1958 <144829504+noob1958@users.noreply.github.com>
2026-03-21 01:24:14 +0100

    Final touches before first release.

--- src/main/java/com/github/noob1958/modlist/maven/plugin/ModlistMojo.java
+++ src/main/java/com/github/noob1958/modlist/maven/plugin/ModlistMojo.java
@@ -2,7 +2,6 @@ package com.github.noob1958.modlist.maven.plugin;
 
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecution;
 import org.apache.maven.plugin.MojoExecutionException;
 
 import org.apache.maven.plugins.annotations.Mojo;
@@ -10,20 +9,27 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
 import javax.inject.Inject;
+import java.io.IOException;
 import java.lang.module.ModuleFinder;
 import java.lang.module.ModuleReference;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.PathMatcher;
 import java.util.List;
 import java.util.Optional;
 
 /**
- * <p>Goal which reads the names of all modules in a folder into a maven session property.</p>
- * <p>Usage:</p>
- * Add these tags to the plugin's configuration.
+ * <p>Goal which reads the names of all modules in a folder into a maven session property. Modules in subfolders are
+ * visited as well.</p>
+ * <p>Add these tags to the plugin's configuration.</p>
  * <ul>
  *     <li>inputDir: the path to the folder to read, relative to the project's base directory. This tag is mandatory.</li>
  *     <li>outputPropertyName: the name of the session property that will receive the output. Optional; the default is "modlist".</li>
+ *     <li>excludeFiles: An optional glob-format string indicating which of the files in the inputDir to exclude before generating
+ *      the module list. E.g. "abc-*-def.jar" will exclude "abc-ghi-def.jar".</li>
+ *     <li>deleteFiles: An optional glob-format string indicating which of the files in the inputDir to delete before generating
+ *      the module list. E.g. "abc-*-def.jar" will delete "abc-ghi-def.jar".</li>
  * </ul>
  */
 @Mojo(name = "list")
@@ -40,15 +46,29 @@ public class ModlistMojo extends AbstractMojo {
     @Parameter(name = "outputPropertyName", property = "outputPropertyName", defaultValue = "modlist")
     private String outputPropertyName;
 
+    /**
+     * An optional glob-format string indicating which of the files in the inputDir to exclude before generating
+     * the module list. E.g. "abc-*-def.jar" will exclude "abc-ghi-def.jar".
+     */
+    @Parameter(name = "excludeFiles", property = "excludeFiles")
+    private String excludeFiles;
+
+    /**
+     * An optional glob-format string indicating which of the files in the inputDir to delete before generating
+     * the module list. E.g. "abc-*-def.jar" will delete "abc-ghi-def.jar".
+     */
+    @Parameter(name = "deleteFiles", property = "deleteFiles")
+    private String deleteFiles;
+
     private final MavenSession session;
     private final MavenProject project;
-    private final MojoExecution mojoExecution;
+    //private final MojoExecution mojoExecution;
 
     @Inject
-    public ModlistMojo(MavenSession session, MavenProject project, MojoExecution mojoExecution) {
+    public ModlistMojo(MavenSession session, MavenProject project) {
         this.session = session;
         this.project = project;
-        this.mojoExecution = mojoExecution;
+        //this.mojoExecution = mojoExecution;
     }
 
     /**
@@ -65,11 +85,11 @@ public class ModlistMojo extends AbstractMojo {
             if (omr.isPresent()) {
                 return omr.get().descriptor().name();
             } else {
-                getLog().warn("Skipped, not a valid JAR or module: " + jarPath.getFileName());
+                getLog().warn("Skipped, not a valid JAR or module: " + jarPath);
                 return "";
             }
         } catch (RuntimeException e) {
-            getLog().error("Skipped because of a problem while reading the file: "+jarPath.getFileName());
+            getLog().error("Skipped because of a problem while reading the file: "+jarPath);
             return "";
         }
     }
@@ -81,11 +101,48 @@ public class ModlistMojo extends AbstractMojo {
         Path inputDirPath = baseDir.resolve(inputDir);
         if (!Files.isDirectory(inputDirPath)) throw new MojoExecutionException("Directory not found: " + inputDir);
         getLog().info("Reading directory "+inputDirPath);
+        PathMatcher excludeMatcher = null;
+        PathMatcher deleteMatcher = null;
+        try {
+            if (excludeFiles != null && !excludeFiles.isBlank()) {
+                excludeMatcher = FileSystems.getDefault().getPathMatcher("glob:"+excludeFiles);
+            }
+        } catch (Exception e) {
+            getLog().warn("Illegal excludeFiles value, no files will be excluded");
+        }
+        try {
+            if (deleteFiles != null && !deleteFiles.isBlank()) {
+                deleteMatcher = FileSystems.getDefault().getPathMatcher("glob:"+deleteFiles);
+            }
+        } catch (Exception e) {
+            getLog().warn("Illegal deleteFiles value, no files will be deleted");
+        }
+        // delete files, then close the first stream to make sure the results have
+        // taken effect when generating the list:
+        try (var stream = Files.list(inputDirPath)) {
+            List<Path> list = stream.toList();
+            for (Path path : list) {
+                if (deleteMatcher != null && deleteMatcher.matches(path.getFileName())) {
+                    try {
+                        Files.deleteIfExists(path);
+                        getLog().info("Deleted: "+path);
+                    } catch (IOException e) {
+                        getLog().warn("Unable to delete file: "+path);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new MojoExecutionException("Unable to read directory "+inputDirPath, e);
+        }
         StringBuilder result = new StringBuilder();
         try (var stream = Files.list(inputDirPath)) {
             List<Path> list = stream.toList();
             String name;
             for (Path path : list) {
+                if (excludeMatcher!=null && excludeMatcher.matches(path.getFileName())) {
+                    getLog().info("Excluded: "+path);
+                    continue;
+                }
                 // fully qualified name
                 name = fullModuleName(path);
                 if (!name.isBlank()) {
