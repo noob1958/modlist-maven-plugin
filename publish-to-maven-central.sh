9d38d5326c49062dd0b4510d24991e24c46b39c5
noob1958 <144829504+noob1958@users.noreply.github.com>
2026-03-22 11:27:55 +0100

    This version was published at Maven Central.

--- publish-to-maven-central.sh
+++ publish-to-maven-central.sh
@@ -3,20 +3,19 @@
 # Contributors have to set the new version before publishing:
 VERSION="1.0.1"
 
-COPY_TO="io/github/noob1958/modlist/$VERSION"
+COPY_TO="io/github/noob1958/modlist-maven-plugin/$VERSION"
 
 ./mvnw clean install -DskipTests=true
 
 echo "Signing artifacts with gpg"
 cd target || exit
-gpg -ab pom.xml
+gpg -ab modlist-maven-plugin-"$VERSION".pom
 gpg -ab modlist-maven-plugin-"$VERSION".jar
 gpg -ab modlist-maven-plugin-"$VERSION"-javadoc.jar
 gpg -ab modlist-maven-plugin-"$VERSION"-sources.jar
 
 echo "Creating and populating folder structure"
 mkdir -pv "$COPY_TO"
-cp pom* "$COPY_TO"
 cp modlist-maven-plugin* "$COPY_TO"
 
 echo "Zipping into a Maven Central bundle"
