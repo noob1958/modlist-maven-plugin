78ef42e0c314494b3cc19444247c08c86858fec9
noob1958 <144829504+noob1958@users.noreply.github.com>
2026-03-22 03:12:56 +0100

    Build script for release to Maven Central completed.

--- publish-to-maven-central.sh
+++ publish-to-maven-central.sh
@@ -3,19 +3,24 @@
 # Contributors have to set the new version before publishing:
 VERSION="1.0.1"
 
-# 1. Build
-echo
-./mvnw clean install -DskipTests
-cp pom.xml target/ || exit
+COPY_TO="io/github/noob1958/modlist/$VERSION"
 
-# 2. Sign artifacts manually
-echo "Starting manual signing..."
+./mvnw clean install -DskipTests=true
+
+echo "Signing artifacts with gpg"
 cd target || exit
 gpg -ab pom.xml
 gpg -ab modlist-maven-plugin-"$VERSION".jar
 gpg -ab modlist-maven-plugin-"$VERSION"-javadoc.jar
 gpg -ab modlist-maven-plugin-"$VERSION"-sources.jar
+
+echo "Creating and populating folder structure"
+mkdir -pv "$COPY_TO"
+cp pom* "$COPY_TO"
+cp modlist-maven-plugin* "$COPY_TO"
+
+echo "Zipping into a Maven Central bundle"
+zip bundle -r io/
 cd ..
 
-# 3. Publish
-# ./mvnw central-publishing:publish -DskipCentralPublishing=false
+echo "Done. Now upload bundle.zip in target/ manually to https://central.sonatype.com/publishing/deployments."
